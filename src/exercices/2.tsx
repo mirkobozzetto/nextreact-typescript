2c13266e4663dc2aafdacc4a865be4170b7dbe27
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-15 16:39:44 +0200

    Add UserNameForm component to handle user names input

--- src/exercices/2.tsx
+++ src/exercices/2.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Board } from "../lib/tictactoe/Board";
+import { UserNameForm } from "../lib/tictactoe/UserNamesForm";
 import {
   calculateNextValue,
   calculateStatus,
@@ -15,6 +16,19 @@ const GameInfo = ({ status }: GameInfoProps) => {
     <div className="game-info">
       <div>{status}</div>
     </div>
+  );
+};
+
+const UserNameForm = ({ onUserNamesSubmitted }: UserNameFormProps) => {
+  return (
+    <form onClick={onSubmit} className="vertical-stack">
+      <h3>Put players usernames</h3>
+      <label htmlFor="user1">User X</label>
+      <input id="user1" ref={userXRef} required minLength={2} />
+      <label htmlFor="user2">User O</label>
+      <input id="user2" ref={userORef} required minLength={2} />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
